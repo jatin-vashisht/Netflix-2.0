8d13b43b5cbc9e6d729da95a6a653a653c4a0b1b
Jatin Vashisht <jatinvashisht11@gmail.com>
2024-03-11 00:49:08 +0530

    Recently Added done

---
+++ app/components/PlayVideoModal.tsx
@@ -0,0 +1,42 @@
+import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle } from "@/components/ui/dialog";
+
+interface iAppProps{
+    title: string;
+    overview: string;
+    youtubeUrl: string;
+    state: boolean;
+    changeState: any;
+    age: number;
+    duration: number;
+    year: number;
+}
+
+export default function PlayVideoModal({
+    changeState,
+    state,
+    title,
+    overview,
+    youtubeUrl,
+    age,
+    duration,
+    release
+}: iAppProps){
+    return (
+        <Dialog open={state} onOpenChange={() => changeState(!state)}>
+            <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                    <DialogTitle>{title}</DialogTitle>
+                    <DialogDescription className="line-clamp-3">
+                        {overview}
+                    </DialogDescription>
+                    <div className="flex gap-x-2 items-center">
+                        <p>{release}</p>
+                        <p className="border py-0.5 px-1 border-gray-200 rounded">{age}+</p>
+                        <p>{duration}h</p>
+                    </div>
+                </DialogHeader>
+                <iframe src={youtubeUrl} height={250} className="w-full"></iframe>
+            </DialogContent>
+        </Dialog>
+    )
+}
